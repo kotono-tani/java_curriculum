01a07a067d0d6745bfd50fcb6feb82056b4575b4
tanikotono <tanikotono@localhost>
2023-05-23 23:41:23 +0900

    課題1.22再提出

--- src/object_program/java1__22.java
+++ src/object_program/java1__22.java
@@ -16,29 +16,15 @@
   */
 package object_program;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import treatment_program.java1_22;
 
-class java1__22 {
+public class java1__22 {
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		
 		// インスタンス化
-		java1_22 theme = new java1_22("こんにちは!", "ここは日本です!", "この寿司はうまい", "寿司は和食です");
-
-		// 出力
-		System.out.println(theme.hello + theme.japan);
-		System.out.println(theme.sushi);
-		System.out.println(theme.food);
-		// 現在日時を取得
-		LocalDateTime nowDate = LocalDateTime.now();
-
-		// 表示形式を指定
-		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); // ①
-		String formatNowDate = dtf1.format(nowDate); // ②
-		System.out.println(formatNowDate);
+		java1_22 consoleOutput = new java1_22();
+		
+		// 実行
+		consoleOutput.printOutput();
 	}
-
 }
