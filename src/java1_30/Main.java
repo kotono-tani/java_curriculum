5b1a65ecf2bb12855e13676b18cf761bc4df9efc
tanikotono <tanikotono@localhost>
2023-06-13 16:46:16 +0900

    課題1.30再提出

--- src/java1_30/Main.java
+++ src/java1_30/Main.java
@@ -2,9 +2,9 @@ package java1_30;
 
 class Main {
 	public static void main(String[] args) {
-		Person person = new Person("鈴木太郎", 20, 1.7, 60);
-		person.print();
-		Person.printNum();
+		Person person = new Person("鈴木太郎", 20, 1.7, 60); // オブジェクトの生成
+		person.print(); // printメソッドを呼ぶ
+		Person.printNum(); // printNumメソッドを呼ぶ
 	}
 
 }
--- src/java1_30/Person.java
+++ src/java1_30/Person.java
@@ -2,16 +2,16 @@ package java1_30;
 
 class Person {
 	// インスタンスフィールドを定義
-	private String name;
-	private int age;
-	private double height;
-	private double weight;
-	private static int num;
+	private String name; // name
+	private int age; // age
+	private double height; // height
+	private double weight; // weight
+	private static int num; // 合計人数num
 
 	// コンストラクタを定義しインスタンスフィールドに値をセット
 	public Person(String name, int age, double height, double weight) { // 修正
 
-		/* 修正 */
+		/* 修正 thisキーワード */
 		this.name = name;
 		this.age = age;
 		this.height = height;
@@ -20,20 +20,19 @@ class Person {
 
 	}
 
-	public double bmi() {
-		return weight / (height * height); // 修正
+	public double bmi() { // インスタンスメソッドを定義
+		return weight / (height * height); // 修正 bmi計算
 	}
 
-	public void print() {
+	public void print() { // インスタンスメソッドを定義
 
 		System.out.println("名前は" + name + "です");
 		System.out.println("年は" + age + "です");
-		String formattedBmi = String.format("%.1f", bmi());
-		System.out.println("BMIは" + formattedBmi + "です");
+		System.out.println("BMIは" + Math.floor(bmi()) + "です"); // 修正
 
 	}
 
-	public static void printNum() {
+	public static void printNum() { // インスタンスメソッドを定義
 		System.out.println("合計" + num + "人です"); // 修正
 
 	}
