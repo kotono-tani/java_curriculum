ed12e79d289f5c9e344b5078e574250ca1dc308d
tanikotono <tanikotono@localhost>
2023-06-26 23:26:43 +0900

    課題1.32再提出

--- src/java1_32/Main.java
+++ src/java1_32/Main.java
@@ -2,24 +2,24 @@ package java1_32;
 
 class Main {
 	public static void main(String[] argos) {
-		
+
 		// オブジェクトの生成
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60, 1);
-		
+
 		// メソッドの呼び出し
 		person1.print();
-		
+
+		// 問題6：クラスメソッド「printCount」を呼び出してください
 		// メソッドの呼び出し
 		person1.printCount();
-		
+
 		// オブジェクトの生成
 		Person person2 = new Person("山田花子", 22, 1.5, 40, 1);
-		
+
 		// メソッドの呼び出し
 		person2.print();
-		
-		// メソッドの呼び出し
-		person2.printCount();
 
+		// 問題3：Main.javaで System.out.printlnを使い「合計〇〇人です」と出力してください。〇〇はcountの値です。
+		System.out.println("合計" + person2.count + "人です"); // 修正
 	}
 }
--- src/java1_32/Person.java
+++ src/java1_32/Person.java
@@ -7,6 +7,8 @@ class Person {
 	public int age;
 	public double height;
 	public double weight;
+
+	// 問題1：クラスフィールド「count」を定義してください（初期値：0,データ型：int）
 	public int count = 0;
 
 	Person(String name, int age, double height, double weight, int count) {
@@ -16,18 +18,15 @@ class Person {
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
-		this.count = count;
+
+		// 問題2：Personコンストラクタの中でクラスフィールドcountに1を足してください
+		this.count = count + 1; // 修正
 
 	}
 
 	// コンストラクタを定義
 	public double bmi() {
 		return this.weight / this.height / this.height;
-	}
-
-	// コンストラクタを定義
-	private int count() {
-		return this.count = 1;
 	}
 
 	// コンストラクタを定義
@@ -40,15 +39,17 @@ class Person {
 		return name;
 	}
 
+	// 問題4：クラスメソッド「printCount」を定義してください（データ型：void）
 	// クラスメソッドを定義
 	public void printCount() {
-		System.out.println("合計" + this.count() + "人です");
+
+		// 問題5：クラスメソッドの中にクラスフィールド「count」を用いて「合計〇〇人です」と出力してください
+		System.out.println("合計" + this.count + "人です");
 	}
 
 	// メソッドを定義
 	public void print() {
 		System.out.println("名前は" + this.name() + "です");
 		System.out.println("年は" + this.age() + "です");
-		System.out.println("合計" + this.count() + "人です");
 	}
 }
