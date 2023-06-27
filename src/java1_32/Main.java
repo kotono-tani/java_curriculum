508c51d792d42712e60e5aba44b63080828f48b7
tanikotono <tanikotono@localhost>
2023-06-27 21:32:31 +0900

    課題1.32再提出

--- src/java1_32/Main.java
+++ src/java1_32/Main.java
@@ -4,7 +4,7 @@ class Main {
 	public static void main(String[] argos) {
 
 		// オブジェクトの生成
-		Person person1 = new Person("鈴木太郎", 20, 1.7, 60, 0);
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 
 		// メソッドの呼び出し
 		person1.print();
@@ -14,12 +14,12 @@ class Main {
 		person1.printCount();
 
 		// オブジェクトの生成
-		Person person2 = new Person("山田花子", 22, 1.5, 40, 1);
+		Person person2 = new Person("山田花子", 22, 1.5, 40);
 
 		// メソッドの呼び出し
 		person2.print();
 
 		// 問題3：Main.javaで System.out.printlnを使い「合計〇〇人です」と出力してください。〇〇はcountの値です。
-		System.out.println("合計" + Person.count + "人です"); // 修正
+		System.out.println("合計" + Person.getCount() + "人です"); // 修正
 	}
 }
--- src/java1_32/Person.java
+++ src/java1_32/Person.java
@@ -9,9 +9,9 @@ class Person {
 	private double weight;
 
 	// 問題1：クラスフィールド「count」を定義してください（初期値：0,データ型：int）
-	public static int count = 0;
+	private static int count = 0;
 
-	Person(String name, int age, double height, double weight, int count) {
+	Person(String name, int age, double height, double weight) {
 
 		// コンストラクタの設定
 		this.name = name;
@@ -20,7 +20,7 @@ class Person {
 		this.weight = weight;
 
 		// 問題2：Personコンストラクタの中でクラスフィールドcountに1を足してください
-		Person.count = count + 1; // 修正
+		Person.setCount(count + 1); // 修正
 
 	}
 
@@ -44,12 +44,20 @@ class Person {
 	public void printCount() {
 
 		// 問題5：クラスメソッドの中にクラスフィールド「count」を用いて「合計〇〇人です」と出力してください
-		System.out.println("合計" + Person.count + "人です"); // 修正
+		System.out.println("合計" + Person.getCount() + "人です"); // 修正
 	}
 
 	// メソッドを定義
 	public void print() {
 		System.out.println("名前は" + this.name() + "です");
 		System.out.println("年は" + this.age() + "です");
+	}
+
+	public static int getCount() {
+		return count;
+	}
+
+	public static void setCount(int count) {
+		Person.count = count;
 	}
 }
